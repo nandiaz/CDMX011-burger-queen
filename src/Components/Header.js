3e413d5b998eaa435aac3108495a0a668ef27cc9
ColetteOrdz <pcoletteor@gmail.com>
2021-11-08 20:30:46 -0600

    more tests with components

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Logo from './assets/Logo_let'
+
+function Header(){
+
+    return (
+        <section>
+            <p>Imagen logo :'0</p>
+            {/* <img src={Logo} alt="Burger-Queen-Logo"/> */}
+        </section>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './Firebase.js';
+import Header from './Components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
